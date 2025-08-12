79cbf687c75af8b3e304a54c4aa0298a70b3fa82
mio200905-oss <mio200905@gmail.com>
2025-08-13 03:47:25 +0900

    mio.lua

--- mio.lua
+++ mio.lua
@@ -1,3 +1,5 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "mio hub / Elemental Power Tycoon💎",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
