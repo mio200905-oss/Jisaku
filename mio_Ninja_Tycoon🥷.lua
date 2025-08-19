3064095b802fb7370a3fe6140d9578250100fa31
mio200905-oss <mio200905@gmail.com>
2025-08-19 20:35:23 +0900

    mio_Ninja_Tycoon🥷.lua

--- mio_Ninja_Tycoon🥷.lua
+++ mio_Ninja_Tycoon🥷.lua
@@ -37,7 +37,7 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
-local MainTab = Window:MainTab("Main", 4483362458)
+local MainTab = Window:CreateTab("Main", 4483362458)
 
 local Button = MainTab:CreateButton({
    Name = "Kaguyai",
