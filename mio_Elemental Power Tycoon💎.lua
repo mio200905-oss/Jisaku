4ed6600883cf16216847f50914cf21396d96a2a6
mio200905-oss <mio200905@gmail.com>
2025-08-19 12:01:43 +0900

    mio_Elemental Power Tycoon💎.lua

--- mio_Elemental Power Tycoon💎.lua
+++ mio_Elemental Power Tycoon💎.lua
@@ -83,6 +83,6 @@ local PlayerTab = Window:CreateTab("Value", 4483362458)
    CurrentValue = 999999,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-                  game:GetService("Players").LocalPlayer.leaderstats.Money = (Value)
-   end,
+   game:GetService("Players").LocalPlayer.leaderstats.Money = (Value)
+end,
 })
