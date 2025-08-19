970d86c09906019e6552cd7bfe1e86bab01c60a2
mio200905-oss <mio200905@gmail.com>
2025-08-19 22:33:47 +0900

    mio_Ninja_Tycoon🥷.lua

--- mio_Ninja_Tycoon🥷.lua
+++ mio_Ninja_Tycoon🥷.lua
@@ -44,7 +44,9 @@ local Toggle = MainTab:CreateToggle({
    CurrentValue = false,
    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
+while wait() do
    workspace.Kaguyai.Humanoid.Health = (0)
+end
    end,
 })
 
@@ -53,7 +55,9 @@ local Toggle = MainTab:CreateToggle({
    CurrentValue = false,
    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
+         while wait() do
    workspace.Nadara.Humanoid.Health = (0)
+         end
    end,
 })
 
@@ -62,6 +66,8 @@ local Toggle = MainTab:CreateToggle({
    CurrentValue = false,
    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
+while wait() do
    workspace.Juubito.Humanoid.Health = (0)
+end
    end,
 })
