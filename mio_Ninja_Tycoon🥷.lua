f3539c1558970bfe362e08ede760ba1a745d887e
mio200905-oss <mio200905@gmail.com>
2025-08-19 21:41:16 +0900

    mio_Ninja_Tycoon🥷.lua

--- mio_Ninja_Tycoon🥷.lua
+++ mio_Ninja_Tycoon🥷.lua
@@ -45,10 +45,3 @@ local Button = MainTab:CreateButton({
    workspace.Kaguyai.Humanoid.Health = (0)
    end,
 })
-
-local Button = MainTab:CreateButton({
-   Name = "Sound Ninja",
-   Callback = function()
-   workspace["Sound Ninja"].Humanoid.Health = (0)
-   end,
-})
