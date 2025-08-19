2389ffee3b5f949b2475f522021cf89c8011c051
mio200905-oss <mio200905@gmail.com>
2025-08-19 11:58:53 +0900

    mio_Elemental Power Tycoon💎.lua

--- mio_Elemental Power Tycoon💎.lua
+++ mio_Elemental Power Tycoon💎.lua
@@ -74,3 +74,15 @@ end,
 })
 
 local PlayerTab = Window:CreateTab("Value", 4483362458)
+
+   local Slider = ValueTab:CreateSlider({
+   Name = "Money",
+   Range = {0, 99999999},
+   Increment = 999999,
+   Suffix = "Money",
+   CurrentValue = 999999,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+                  game:GetService("Players").LocalPlayer.leaderstats.Money = (Value)
+   end,
+})
